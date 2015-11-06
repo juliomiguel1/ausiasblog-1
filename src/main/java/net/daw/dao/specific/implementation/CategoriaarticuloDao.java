e0ea7498fea04327ad4848a04289a17c5359efc5
a022583461z <a022583461z@s09-pc05>
2015-11-06 10:00:36 +0100

    Metodos especificos servidor blog

--- src/main/java/net/daw/dao/specific/implementation/CategoriaarticuloDao.java
+++ src/main/java/net/daw/dao/specific/implementation/CategoriaarticuloDao.java
@@ -46,7 +46,77 @@ public class CategoriaarticuloDao extends TableDaoGenImpl<CategoriaarticuloBean>
     public CategoriaarticuloDao(Connection pooledConnection) throws Exception {
         super(pooledConnection);
     }
-   
 
+    @Override
+    public CategoriaarticuloBean get(CategoriaarticuloBean oCategoriaArticuloBean, Integer expand) throws Exception {
+        if (oCategoriaArticuloBean.getId() > 0) {
+            if (oMysql.existsOne(strSqlSelectDataOrigin, oCategoriaArticuloBean.getId())) {
+                oCategoriaArticuloBean.setNombre(oMysql.getOne(strSqlSelectDataOrigin, "nombre", oCategoriaArticuloBean.getId()));
+                oCategoriaArticuloBean.setDescripcion(oMysql.getOne(strSqlSelectDataOrigin, "descripcion", oCategoriaArticuloBean.getId()));
+            }
+        }
+        return oCategoriaArticuloBean;
+    }
+
+    @Override
+    public ArrayList<CategoriaarticuloBean> getAll(ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder) throws Exception {
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlWhere(alFilter);
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlOrder(hmOrder);
+        ResultSet oResultset = oMysql.getAllSql(strSqlSelectDataOrigin);
+        ArrayList<CategoriaarticuloBean> alCategorias = new ArrayList<>();
+        while (oResultset.next()) {
+            int id = oResultset.getInt("id");
+            CategoriaarticuloBean oCategoriaArticuloBean = new CategoriaarticuloBean();
+            oCategoriaArticuloBean.setId(id);
+            oCategoriaArticuloBean.setNombre(oMysql.getOne(strSqlSelectDataOrigin, "nombre", id));
+            oCategoriaArticuloBean.setDescripcion(oMysql.getOne(strSqlSelectDataOrigin, "descripcion", id));
+            alCategorias.add(oCategoriaArticuloBean);
+        }
+        return alCategorias;
+    }
+
+    public int getCount(ArrayList<FilterBeanHelper> alFilter) throws Exception {
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlWhere(alFilter);
+        return oMysql.getCount(strSqlSelectDataOrigin);
+
+    }
+
+    @Override
+    public ArrayList<CategoriaarticuloBean> getPage(int intRegsPerPag, int intPage, ArrayList<FilterBeanHelper> alFilter, HashMap<String, String> hmOrder) throws Exception {
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlWhere(alFilter);
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlOrder(hmOrder);
+        ResultSet oResultset = oMysql.getPage(strSqlSelectDataOrigin, intRegsPerPag, intPage);
+        ArrayList<CategoriaarticuloBean> alCategorias = new ArrayList<>();
+        while (oResultset.next()) {
+            int id = oResultset.getInt("id");
+            CategoriaarticuloBean oCategoriaArticuloBean = new CategoriaarticuloBean();
+            oCategoriaArticuloBean.setId(id);
+            oCategoriaArticuloBean.setNombre(oMysql.getOne(strSqlSelectDataOrigin, "nombre", id));
+            oCategoriaArticuloBean.setDescripcion(oMysql.getOne(strSqlSelectDataOrigin, "descripcion", id));
+            alCategorias.add(oCategoriaArticuloBean);
+        }
+        return alCategorias;
+    }
+
+    @Override
+    public int getPages(int intRegsPerPag, ArrayList<FilterBeanHelper> alFilter) throws Exception {
+        strSqlSelectDataOrigin += SqlBuilder.buildSqlWhere(alFilter);
+        return oMysql.getPages(strSqlSelectDataOrigin, intRegsPerPag);
+    }
+
+    @Override
+    public CategoriaarticuloBean set(CategoriaarticuloBean oCategoriaArticuloBean) throws Exception {
+        if (oCategoriaArticuloBean.getId() == 0) {
+            oCategoriaArticuloBean.setId(oMysql.insertOne(strTableOrigin));
+        }
+        oMysql.updateOne(oCategoriaArticuloBean.getId(), strTableOrigin, "nombre", oCategoriaArticuloBean.getNombre());
+        oMysql.updateOne(oCategoriaArticuloBean.getId(), strTableOrigin, "descripcion", oCategoriaArticuloBean.getDescripcion());
+        return oCategoriaArticuloBean;
+    }
+
+    @Override
+    public int remove(CategoriaarticuloBean oCategoriaArticuloBean) throws Exception {
+        return oMysql.removeOne(oCategoriaArticuloBean.getId(), strTableOrigin);
+    }
 
 }
