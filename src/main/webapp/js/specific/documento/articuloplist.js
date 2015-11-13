99e0e6d24863c9fa2b590d85a7c798c9ca9c527c
a026754289e <a026754289e@s09-pc06>
2015-11-13 10:09:48 +0100

    cliente estructurado

--- src/main/webapp/js/specific/documento/articuloplist.js
+++ src/main/webapp/js/specific/documento/articuloplist.js
@@ -33,22 +33,30 @@ articuloplist.prototype = new pListModule();
 articuloplist.prototype.render = function () {
     if (jsonData.status == 200) {
         var entrada = "";
-
+        
         entrada += '<div class = "entradas">';
         for (var i = 0; i <= jsonData.message.page.message.length - 1; i++) {
 
             entrada += '<div class = "blog">';
             entrada += '<div class = "titulo">' + jsonData.message.page.message[i].titulo + '</div>' + '</br>';
-            entrada += '<div>' + jsonData.message.page.message[i].contenido + '</div>';
-            entrada += '<div>' + jsonData.message.page.message[i].alta + '</div>';
+            entrada += '<div class = "contenido">' + jsonData.message.page.message[i].contenido + '</div>';
+            entrada += '<div class = "datos">' + "Fecha de alta: " + jsonData.message.page.message[i].alta + " | ";
             //autor += jsonData.message.page.message[i].autor;
-            entrada += '<div>' + jsonData.message.page.message[i].hits + '</div>';
-            entrada += '<div>' + jsonData.message.page.message[i].etiquetas + '</div>';
+            entrada += "Visitas: " + jsonData.message.page.message[i].hits + " | ";
+            entrada += "Tags: " + jsonData.message.page.message[i].etiquetas + '</div>';
             entrada += '</div>';
+            
         }
         return  entrada;
         entrada += '</div>';
+        
+        var categoria = "";
+        categoria += '<div class = "categoria">';
+        categoria += '<input type="text" class="query" name="q" size="21" maxlength="255" value=" Búsqueda por categoría" onclick="this.select()">';
+        categoria += '</div>';
     }
     ;
+    
+
 
 };
